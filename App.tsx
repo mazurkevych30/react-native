aee525df0a70b664881fb5e7b706bc0dc7cc9cf5
mazurkevych30 <132198562+mazurkevych30@users.noreply.github.com>
2024-11-08 01:46:57 +0000

    test push

---
+++ App.tsx
@@ -0,0 +1,39 @@
+// import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+
+import LoginSceen from "./screens/LoginSceen";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  // if (!fontsLoaded) {
+  //   return <ActivityIndicator size={"large"} />;
+  // }
+
+  return <LoginSceen />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ screens/LoginSceen.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, View } from "react-native";
+
+const LoginSceen = () => {
+  return <View></View>;
+};
+
+export default LoginSceen;
+
+const style = StyleSheet.create({});
